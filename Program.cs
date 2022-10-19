6bd6b64407bcd0a9b71ebe29282918402f2ba2e0
Rajashekar Vemula <vemula.rajashekar02@gmail.com>
2022-10-19 19:46:10 +0530

    added Logical Programs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using LogicalPrograms;
+
+namespace LogialPrograms
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Logical Program");
+        }
+    }
+}
